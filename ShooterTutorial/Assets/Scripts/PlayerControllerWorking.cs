8238eacf06cf76aaba9fe10dfc7735583a2b3be9
wfahnbul <wfahnbul@wellesley.edu>
2017-02-22 16:51:12 -0500

    PlayerController
    The one that works horizontally. May need to be renamed...

---
+++ ShooterTutorial/Assets/Scripts/PlayerControllerWorking.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public GameObject PlayerBullet;
+    public GameObject bulletPosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject bullet = (GameObject)Instantiate(PlayerBullet);
+            bullet.transform.position = bulletPosition.transform.position;
+
+        }
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        Vector2 direction = new Vector2(x, y).normalized;
+
+        Move(direction);
+
+	}
+
+    void Move(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        max.x = max.x - 0.225f;
+        min.x = min.x + 0.225f;
+
+        max.y = max.y - 0.600f;
+        min.y = min.y + 0.600f;
+
+        Vector2 pos = transform.position;
+
+        pos += direction * speed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, min.y);
+
+        transform.position = pos;
+            
+            }
+
+
+}
