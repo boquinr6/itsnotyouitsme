7669bad03b4aa83c08da2aefa470dbce481b02a1
Elise <eyxue@mit.edu>
2017-02-27 14:30:35 -0500

    added new sprites to particle effects, new build version

--- ShooterTutorial/Assets/2DSpaceShooterExample/CompleteProject/Scripts/GameControllerScript.cs
+++ ShooterTutorial/Assets/2DSpaceShooterExample/CompleteProject/Scripts/GameControllerScript.cs
@@ -9,6 +9,7 @@ public class GameControllerScript : MonoBehaviour
 	public GameObject playerShip;
 	public GameObject playButton;
 	public GameObject enemySpawner;
+	public GameObject enemyShip;
 
 	public enum GameManagerState {
 		Opening,
@@ -32,17 +33,26 @@ public class GameControllerScript : MonoBehaviour
 
 	void UpdateGameManagerState() {
 		switch (GMState) {
-			case GameManagerState.Opening:
-				playButton.SetActive (true);
-				break;
-			case GameManagerState.Gameplay:
-				playButton.SetActive (false);
+		case GameManagerState.Opening:
+			playButton.SetActive (true);
+			playerShip.SetActive (false);
+			break;
+		case GameManagerState.Gameplay:
+			playButton.SetActive (false);
+			enemyShip.SetActive (true);
+			playerShip.SetActive (true);
 //				playerShip.GetComponent<EnemyControl> ().Init ();
-				enemySpawner.GetComponent<spawnScript> ().ScheduleEnemySpawner();
-				break;
-			case GameManagerState.Gameover:
-				enemySpawner.GetComponent<spawnScript> ().UnscheduleEnemySpawner ();
-				break;
+			enemySpawner.GetComponent<spawnScript> ().ScheduleEnemySpawner();
+			break;
+		case GameManagerState.Gameover:
+			enemySpawner.GetComponent<spawnScript> ().UnscheduleEnemySpawner ();
+			enemyShip.SetActive (false);
+			//Stops game
+			Time.timeScale = 0;
+			//quits app
+//			Application.Quit();
+			Invoke ("ChangeToOpeningState", 1f);
+			break;
 		}
 	}
 
